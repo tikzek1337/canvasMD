43283aaf0b4bbdc70c354472b5eaedf24b3ee5d1
Timofey <mrfanta228timz@gmail.com>
2026-06-27 02:15:41 +0300

    Delete src directory

--- src/utils/file.ts
+++
@@ -1,8 +0,0 @@
-export function readFileAsDataUrl(file: File) {
-  return new Promise<string>((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = () => resolve(String(reader.result));
-    reader.onerror = () => reject(reader.error);
-    reader.readAsDataURL(file);
-  });
-}
