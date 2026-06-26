b40416a9ef645198cda6f75e2d45137b327c36a9
Timofey <mrfanta228timz@gmail.com>
2026-06-27 02:15:30 +0300

    0

--- electron/preload.ts
+++
@@ -1,6 +0,0 @@
-import { contextBridge, ipcRenderer } from 'electron';
-
-contextBridge.exposeInMainWorld('canvasMDApi', {
-  saveJson: (payload: string) => ipcRenderer.invoke('project:saveJson', payload),
-  openJson: () => ipcRenderer.invoke('project:openJson')
-});
